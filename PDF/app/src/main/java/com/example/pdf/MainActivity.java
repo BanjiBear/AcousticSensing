efb3cda887b679b5493ec2efc0b73c22887fa211
BanjiBear <tommychen900217@gmail.com>
2022-05-18 15:07:20 +0800

    websocket: error permission denied FIXED (Client Connection DONE !!!!!)

--- PDF/app/src/main/java/com/example/pdf/MainActivity.java
+++ PDF/app/src/main/java/com/example/pdf/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.pdf;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
